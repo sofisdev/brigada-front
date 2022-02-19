60266f0e1d1f543bd1674f31daf116a6fa341caf
SofSanUrb <sofiasanchezurbano@gmail.com>
2022-02-19 19:18:57 +0100

    fix: redirect from home page

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -10,7 +10,6 @@ const isAppRoute = (pathname) =>
     .find((route) => route.startsWith(pathname));
 
 export async function middleware(req = NextRequest) {
-  console.log("🤓🤓 ~ file: _middleware.js ~ line 13 ~ middleware ~ req", req)
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
@@ -18,15 +17,12 @@ export async function middleware(req = NextRequest) {
       process.env.NEXTAUTH_URL?.startsWith('https://') ??
       process.env.NEXTAUTH_URL,
   });
-  console.log("🚀 ~ file: _middleware.js ~ line 21 ~ middleware ~ token", token)
   const { pathname } = req.nextUrl;
-  console.log("🔥🔥 ~ file: _middleware.js ~ line 23 ~ middleware ~ pathname", pathname)
   if (pathname.includes(routes.api) || !!token || !isAppRoute(pathname)) {
     return NextResponse.next();
   }
 
   if (!token && pathname !== routes.login) {
-    console.log('TEST LOG ✅');
-    return NextResponse.rewrite(new URL(`/${routes.login}`, req.url));
+    return NextResponse.redirect(`${routes.login}`);
   }
 }
