9068b35698db16483cf276fdb93437f7a46cdc81
sofisdev <sofia.sanchez@auctane.com>
2022-07-13 18:47:25 +0200

    feat: new meta description

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ const MyApp = function ({ Component, pageProps }) {
         <meta name="description" content="Web de registro para invitados" />
         <meta property="og:title" content="Somos brigada" />
         <meta property="og:url" content="https://www.somosbrigada.net/" />
-        <meta property="og:type" content="website">
+        <meta property="og:type" content="website" />
         <meta property="og:description" content="Web de registro para invitados" />
         <meta property="og:image" content="https://aalcachucho.com/wp-content/uploads/eventos-fiestas-bodas-cumpleanos-bautizos-aniversario-carpa-circo-sierra-madrid-aalcachucho.jpg" />
         <meta name="googlebot" content="notranslate"/>
